156e230dc41c2eb91fb2e69a7f86bcf48bdf1ec6
Victor Ciolac <victor.bcfraga@gmail.com>
2024-02-01 16:09:22 -0300

    Req. 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    txt_list = txt_importer(path_file)
+    for data in instance.queue:
+        if data["nome_do_arquivo"] == path_file:
+            print(f"Arquivo {path_file} já foi processado.")
+            return
+
+    dict_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_list),
+        "linhas_do_arquivo": txt_list,
+    }
+
+    instance.enqueue(dict_data)
+    print(f"Dados processados do arquivo {path_file}:\n{dict_data}")
 
 
 def remove(instance):
