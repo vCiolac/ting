7b4c34789569796ebe909d472dbc792a5b7ce252
Victor Ciolac <victor.bcfraga@gmail.com>
2024-02-01 15:27:02 -0300

    Req. 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+        return
+    with open(path_file, "r") as file:
+        return file.read().splitlines()
