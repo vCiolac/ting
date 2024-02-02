6a86abf392a78cd2a1a2c879ed28392a0f4228b3
Victor Ciolac <victor.bcfraga@gmail.com>
2024-02-02 17:32:47 -0300

    teste req.7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for index in range(len(instance)):
+        ocorrenias = list()
+        queueSearch = instance.search(index)
+        txt_file = txt_importer(queueSearch)
+
+        for i in range(len(txt_file)):
+            if word in txt_file[i].lower():
+                ocorrenias.append({"linha": i + 1})
+
+        if len(ocorrenias) == 0:
+            return []
+
+        result.append({
+                "palavra": word,
+                "arquivo": queueSearch,
+                "ocorrenias": ocorrenias
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
